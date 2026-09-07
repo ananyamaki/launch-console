14f1d2efc2d36368f3446e79a0919d9bc183e135
ananyamaki <ananyamakineedi@gmail.com>
2026-09-07 21:27:07 +0000

    Launch Console introductory code

--- launch_console.py
+++ launch_console.py
@@ -1,3 +1,24 @@
-print("Hi, my name is Ananya.")
-print("My goals: To successfully ship my first project this semester and use GitHub properly.")
-print("About Me: I am currently in 10th grade at McNeil High School in Austin, TX.")
+name = input("What is you name?: ")
+
+print(f'Welcome, {name} to the Launch Console!')
+def display_console():
+    print("1) About Me")
+    print("2) My Goals")
+    print("3) Fun Fact")
+    print("4) Exit")
+
+while True:
+    display_console()
+    choice = input("Pick an option: ")
+
+    if choice == "1":
+        about_me = input("About Me: ")
+    elif choice == "2":
+        my_goals = input("My Goals: ")
+    elif choice == "3":
+        fun_fact = input("Fun fact about me: ")
+    elif choice == "4":
+        print("Goodbye!")
+        break
+    else:
+        print("Invalid input. Please pick a valid number.")
