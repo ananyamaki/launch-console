0625b95c16979b1d23cf5ae44cc70c11605ca7f1
ananyamaki <ananyamakineedi@gmail.com>
2026-09-07 17:54:43 +0000

    Introducing myself

---
+++ launch_console.py
@@ -0,0 +1,3 @@
+print("Hi, my name is Ananya.")
+print("My goals: To successfully ship my first project this semester and use GitHub properly.")
+print("About Me: I am currently in 10th grade at McNeil High School in Austin, TX.")
